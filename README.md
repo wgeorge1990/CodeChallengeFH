77939a150362cb3864ae9dbc8fe86356510b9b2c
William George (wgeorge1990) <williameverettgeorge@icloud.com>
2021-02-19 14:04:39 -0500

    First round of refactoring from original rough solution

--- README.md
+++
@@ -1,2 +0,0 @@
-# CodeChallengeFH
-Code Challenge
--- index.js
+++ index.js
@@ -10,48 +10,41 @@ function filterRangeForPrimes(range) {
 
 function isPrime(num) {
     if (num === 1) return false
-    if (num === 2) {
-        return true;
-    } else {
+        while(num === 2) return true
         for (let x = 2; x < num; x++) {
             if (num % x === 0) return false
         }
         return true;
     }
-}
 
-function getUserInput() {
-    const ReadLine = require("readline").createInterface({
+const ReadLine = require("readline").createInterface({
         "input": process.stdin,
         "output": process.stdout
     })
 
+    const getFreqOfDigits = (primesArray) => primesArray.reduce((acc, currVal) => {
+        return acc.toString().concat(currVal.toString())
+        }).split("").reduce((acc, currVal) => {
+        if (currVal in acc) {
+            acc[currVal]++
+        } else {
+            acc[currVal] = 1
+        }
+        return acc
+        }, {})
+
+function getUserInput() {
     ReadLine.question(`Please enter start and end of range using (example: 1 20) with one space in between: `, (input) => {
+
         console.warn("Input starts and ends with a number => ( ",
         input.trim().startsWith(0, typeof Number).toString().endsWith(typeof Number) + " )")
-        let isValidInput = ((input.includes(" ") && (input.length > 2)))
-            console.log("Is your input valid?", isValidInput ? " it is valid" : " ,something is wrong..")
-        if (!isValidInput) {
-            console.log("Oops, something went wrong with the input. Please try again. Usually caused by to many spaces in between the TWO range characters.");
-            getUserInput().trim(" ")
-            if (input === 'exit') return close()
-        } else {
+
             let start = parseInt(input.split(" ")[0])
             let end = parseInt(input.split(" ")[1])
             let range = createRange(start, end);
             let primes = filterRangeForPrimes(range);
+            let freqOfDigits = getFreqOfDigits(primes)
             
-            const freqOfDigits = primes.reduce((acc, currVal) => {
-                return acc.toString().concat(currVal.toString())
-                }).split("").reduce((acc, currVal) => {
-                if (currVal in acc) {
-                    acc[currVal]++
-                } else {
-                    acc[currVal] = 1
-                }
-                return acc
-                }, {}) 
-
                 console.log("Frequency of Prime numbers: ", freqOfDigits)
                 
                 let freqArr = []
@@ -73,8 +66,7 @@ function getUserInput() {
                 
                 const answer = Math.max(...listOfHighestOccurringDigits)
                 console.log("\nAnswer: ", answer)
-        
-        }
+            
     })
 };
 
